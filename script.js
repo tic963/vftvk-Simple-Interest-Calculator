9a4205bce9809b9820295b7297f2b949845dc90e
tic963 <tienc963@outlook.com>
2021-12-09 15:33:01 -0800

    homework-v2

--- script.js
+++ script.js
@@ -20,22 +20,29 @@ updateRanchRateValue();
 
 
 //update number of year
-let yearNum = document.getElementById("years").value;
+let yearNum = document.getElementById("years");
 
 //calculate yearly interest rate
 let showResultText = document.getElementById("showResult");
 let compute = () => {
-    return principalInput.value * (((rangeRateValue.value / 100) * yearNum));
+    return principalInput.value * (((rangeRateValue.value / 100) * yearNum.value));
 }
 
+
 const d = new Date();
-let year = d.getFullYear();
-let endYear = parseInt(year) + parseInt(yearNum);
+let year1 = d.getFullYear();
+let yearNumShow = parseInt(yearNum.value,10);
+let endYear = year1 + yearNum.value;
+
+let dueDate = () => {
+    return Number(yearNum.value) + Number(year1);
+}
+
 console.log(compute());
 let showCalculationResult = () => {
     let grossAmount = parseInt(compute());
     showResultText.innerHTML = `if you deposti ${principalInput.value},<br>
     at an interest rate of ${rangeRateValue.value}%.<br>
     You will receive an amount of ${grossAmount}.<br>
-    in the year ${endYear}`
+    in the year ${ dueDate()}`
 }
